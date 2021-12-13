04b66e411a67b80e66a0a3947c9bc0f5184cf6f1
João Victor Fernandes de Araújo <joaovictorfernandesdearaujo@gmail.com>
2021-12-13 19:14:07 -0300

    Update Usuario.php

--- feedcheer.mypressonline.com/feed/model/Usuario.php
+++ feedcheer.mypressonline.com/feed/model/Usuario.php
@@ -68,25 +68,14 @@ class Usuario{
            </script>"
            );
             
-           
-           
 		}
 	}
 
-
-    /**
-     * Get the value of id
-     */ 
     public function getId()
     {
         return $this->id;
     }
 
-    /**
-     * Set the value of id
-     *
-     * @return  self
-     */ 
     public function setId($id)
     {
         $this->id = $id;
@@ -94,19 +83,11 @@ class Usuario{
         return $this;
     }
 
-	/**
-	 * Get the value of nome
-	 */ 
 	public function getNome()
 	{
 		return $this->nome;
 	}
 
-	/**
-	 * Set the value of nome
-	 *
-	 * @return  self
-	 */ 
 	public function setNome($nome)
 	{
 		$this->nome = $nome;
@@ -114,19 +95,11 @@ class Usuario{
 		return $this;
 	}
 
-    /**
-     * Get the value of email
-     */ 
     public function getEmail()
     {
         return $this->email;
     }
 
-    /**
-     * Set the value of email
-     *
-     * @return  self
-     */ 
     public function setEmail($email)
     {
         $this->email = $email;
@@ -134,19 +107,11 @@ class Usuario{
         return $this;
     }
 
-	/**
-	 * Get the value of telefone
-	 */ 
 	public function getTelefone()
 	{
 		return $this->telefone;
 	}
 
-	/**
-	 * Set the value of telefone
-	 *
-	 * @return  self
-	 */ 
 	public function setTelefone($telefone)
 	{
 		$this->telefone = $telefone;
@@ -154,19 +119,11 @@ class Usuario{
 		return $this;
 	}
 
-	/**
-	 * Get the value of mensagem
-	 */ 
 	public function getMensagem()
 	{
 		return $this->mensagem;
 	}
 
-	/**
-	 * Set the value of mensagem
-	 *
-	 * @return  self
-	 */ 
 	public function setMensagem($mensagem)
 	{
 		$this->mensagem = $mensagem;
